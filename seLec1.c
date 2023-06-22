3284ae7fc9c2ef737460d10b3f07a733fbd055e4
Darshanproject <gadadarshan6184@gmail.com>
2023-06-22 16:24:27 +0530

    Done

---
+++ seLec1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int num,num1;
+	printf("Enter your values :");
+	scanf("%d",&num);
+	printf("Enter your second value :");
+	scanf("%d",&num1);
+	printf("This is your addtion %d + %d = %d",num,num1,num+num1);
+}
