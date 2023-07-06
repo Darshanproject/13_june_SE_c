90e1c4c3ff52917aea1218020df1031cf455a489
Darshanproject <gadadarshan6184@gmail.com>
2023-07-06 19:53:25 +0530

    Done

---
+++ Lecture3/part1.c
@@ -0,0 +1,19 @@
+/*
+Syntax :
+int
+ while(Condtion){
+ body of loop
+ updation;
+ }
+*/
+
+#include<stdio.h>
+main()
+{
+	int i=10;
+	while(i>=0)
+	{
+		printf("%d \n",i);
+		i--;
+	}
+}
---
+++ Lecture3/part2.c
@@ -0,0 +1,16 @@
+/*
+Syntax:
+do{
+body of loop
+updation;
+}while(condtion);
+*/
+#include<stdio.h>
+main()
+{
+	int i=1;
+	do{
+		printf("%d \n",i);
+		i++;
+	}while(i<=10);
+}
